b2149123b3b08fc0e6b0e71a8cf88544b0276519
Felice Forby <feliceforby@gmail.com>
2018-04-14 06:33:29 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+# Using #each
+# def reverse_each_word(string)
+#   words = string.split(" ")
+#   reversed_words = []
+#   words.each{ |word| reversed_words << word.reverse }
+#   return reversed_words.join(" ")
+# end
+
+# Using #collect
+def reverse_each_word(string)
+  words = string.split(" ")
+  reversed_words = words.collect{ |word| word.reverse }
+  reversed_words.join(" ")
+end
